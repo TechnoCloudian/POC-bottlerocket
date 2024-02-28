581ac15496d0915c1b71c8d495aa5ae2aa166dbc
Murad Ali <141018381+TechnoCloudian@users.noreply.github.com>
2024-02-28 12:57:55 +0000

    refector the terraform code

---
+++ terraform/policy.tf
@@ -0,0 +1,41 @@
+# IAM Policy for Cluster
+resource "aws_iam_policy" "cluster_policy" {
+  name = "eks-cluster-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:PassRole",
+        "iam:CreateServiceLinkedRole",
+        "ec2:DescribeSecurityGroups",
+        "ec2:CreateSecurityGroups",
+        "ec2:DeleteSecurityGroups",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DescribeSubnets",
+        "ec2:CreateNetworkInterface",
+        "ec2:DeleteNetworkInterface",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetGroupAttributes",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeRules",
+        "elasticloadbalancing:ModifyTargetGroupAttributes",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchTemplates",
+        "autoscaling:CreateAutoScalingGroup"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
